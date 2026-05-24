f23a871088536c6f4145a2befec58c018e4cef06
Will Tekulve <tekulve.will@gmail.com>
2026-05-24 14:04:20 -0700

    chore(ui): scaffold SvelteKit SPA with static adapter

---
+++ ui/vite.config.ts
@@ -0,0 +1,17 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  plugins: [sveltekit()],
+  server: {
+    proxy: {
+      '/api':   { target: 'http://localhost:8080', changeOrigin: true },
+      '/icons': { target: 'http://localhost:8080', changeOrigin: true },
+    },
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['./src/test-setup.ts'],
+  },
+});
